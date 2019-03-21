7a9288aa6e3876438aecd905f0b5342b2f02fb29
Christopher Anthony <nator456@aol.com>
2019-03-21 17:14:55 -0400

    first game project

---
+++ simongame.js
@@ -0,0 +1,11 @@
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var randomChosenColour = buttonColours[nextSequence()];
+
+function nextSequence(){
+    var randomNumber = Math.floor((Math.random() * 4));
+    return randomNumber
+
+}
+console.log(randomChosenColour);
